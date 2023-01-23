bee540f73f1ca1353e79aff8b3dafc335a11b581
jewel <snowdecember079@gmail.com>
2023-01-23 00:15:29 -0500

    commit tree 76efdf3c5e67c114ef7cfebc5befff14c8cb5993

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -12,13 +12,29 @@ class PlantsController < ApplicationController
     render json: plant
   end
 
+ 
+
+
   # POST /plants
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
 
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  def destroy
+ plant = Plant.find_by(id: params[:id])
+   plant.destroy
+    head :no_content
+  end
+
   private
+ 
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
